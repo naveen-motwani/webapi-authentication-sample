592fb3cceaee2af77edb24b184ee249bbc9f5466
naveen-arcadis <naveen.motwani@arcadis.com>
2022-02-15 16:36:50 +0530

    Replaced client id with application url

--- Program.cs
+++ Program.cs
@@ -5,26 +5,20 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
-
-
-//// Add services to the container.
-//builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration);
-////builder.Services.AddAuthentication()
-
 var applicationURL = "weatherapipoc.azurewebsites.net";
-var authorization_uri= "https://login.windows.net/85fb7079-89c1-4496-8137-4cc295758328/oauth2/v2.0/authorize";
+var authorization_uri = "https://login.windows.net/85fb7079-89c1-4496-8137-4cc295758328/oauth2/v2.0/authorize";
 var clientId = "13c6a116-55ea-462a-a19c-6a493917128b";
 var tenantId = "85fb7079-89c1-4496-8137-4cc295758328";
-//var instance = "https://login.microsoftonline.com";
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(opt =>
 {
     opt.Challenge = $"Bearer realm=\"{applicationURL}\" authorization_uri=\"{authorization_uri}\" resource_id=\"{clientId}\"";
+
 },
 opt =>
 {
     opt.TenantId = tenantId;
-    opt.ClientId = clientId;
+    opt.ClientId = applicationURL;
     opt.Instance = "https://login.microsoftonline.com";
 });
 
